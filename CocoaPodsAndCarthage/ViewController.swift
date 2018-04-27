e70421a8bcea6b14e5a007812076fd7590cc0135
yangyang.cao <yangyang.cao@oqiji.com>
2018-04-27 16:53:40 +0800

    CocoaPodsAndCarthage

---
+++ CocoaPodsAndCarthage/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CocoaPodsAndCarthage
+//
+//  Created by 曹洋洋 on 2018/4/27.
+//  Copyright © 2018年 yang. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '8.0'
+use_frameworks!
+target 'CocoaPodsAndCarthage' do 
+ pod 'SnapKit', '~> 4.0.0'
+end
